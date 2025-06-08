c56b456ea6ded9d82a2ea8adc2e1271c40496928
ssollnncce <126074818+ssollnncce@users.noreply.github.com>
2025-06-08 18:07:38 +0500

    change cors and api routes

--- frontend-pxb/src/api.js
+++ frontend-pxb/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:80/api', // URL к Laravel API
+  baseURL: 'http://profxbspuadmin.ssollnncce.ru/api', // URL к Laravel API
   withCredentials: true, // если используешь куки и sanctum
 });
 
